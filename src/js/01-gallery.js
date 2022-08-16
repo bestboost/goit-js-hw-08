d3e6de69fc1e90d90c41d46e88415f568e479c03
Darya Hudzovska <1bestboost@gmail.com>
2022-08-16 18:42:52 +0300

    test

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -7,8 +7,6 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
-
 
 const refs = document.querySelector('.gallery');
 
