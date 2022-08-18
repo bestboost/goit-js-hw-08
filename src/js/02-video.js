b8fa92b93211d631672902646ac7fc12edd66e41
Darya Hudzovska <1bestboost@gmail.com>
2022-08-18 18:36:08 +0300

    finish task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,53 +1,18 @@
-// В HTML есть <iframe> с видео для Vimeo плеера. Напиши скрипт который будет сохранять текущее время воспроизведения видео в локальное 
-// хранилище и, при перезагрузке страницы, продолжать воспроизводить видео с этого времени.
-
-// <iframe
-//   id="vimeo-player"
-//   src="https://player.vimeo.com/video/236203659"
-//   width="640"
-//   height="360"
-//   frameborder="0"
-//   allowfullscreen
-//   allow="autoplay; encrypted-media"
-// ></iframe>
-// Выполняй это задание в файлах 02-video.html и 02-video.js. Разбей его на несколько подзадач:
-
-
-
-
-
-
-
-// Разбери документацию метода on() и начни отслеживать событие timeupdate - обновление времени воспроизведения.
-
-// Сохраняй время воспроизведения в локальное хранилище. Пусть ключом для хранилища будет строка "videoplayer-current-time".
-
-// При перезагрузке страницы воспользуйся методом setCurrentTime() для того чтобы возобновить воспроизведение с сохраненной позиции.
-
-// Добавь в проект бибилотеку lodash.throttle и сделай так, чтобы время воспроизведения обновлялось в хранилище не чаще чем раз в секунду.
-
+import throttle from 'lodash.throttle';
 import Player from '@vimeo/player';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('play', function() {
-    console.log('played the video!');
-});
+const onPlay = function({seconds}) {
 
-const onPlay = function(timeupdate) {
-    // data is an object containing properties specific to that event
-    console.log(timeupdate)
- 
-   
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(seconds));
+
 };
 
-player.on('play', onPlay);
+player.on('timeupdate', throttle(onPlay, 1000));
 
+const sevedTimeOnLocal = localStorage.getItem("videoplayer-current-time");
 
-localStorage.setItem("videoplayer-current-time", onPlay)
-
-
-
-
+player.setCurrentTime(sevedTimeOnLocal);
 
