0a6d9d724926c58b02e01cdb832cf169f04bfd61
Darya Hudzovska <1bestboost@gmail.com>
2022-08-21 20:15:27 +0300

    bugfix from mentor task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -14,5 +14,18 @@ player.on('timeupdate', throttle(onPlay, 1000));
 
 const sevedTimeOnLocal = localStorage.getItem("videoplayer-current-time");
 
-player.setCurrentTime(sevedTimeOnLocal);
+player.setCurrentTime(sevedTimeOnLocal).then(function(sevedTimeOnLocal) {
+    
+    player.off('play')
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            console.error('the time was less than 0 or greater than the video’s duration')
 
+            break;
+
+        default:
+            console.error('some other error occurred')
+            break;
+    }
+});
