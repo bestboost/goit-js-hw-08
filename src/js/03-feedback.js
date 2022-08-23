0c9be9c324a5d9755b1400b08e2f473b37eb4a9f
Darya Hudzovska <1bestboost@gmail.com>
2022-08-23 19:47:18 +0300

    paste info from master branch

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,46 +1,63 @@
 import throttle from 'lodash.throttle';
 
-const STORAGE_KAY = 'feedback-form-state';
+const STORAGE_KEY = 'feedback-form-state';
 
 const form = document.querySelector('.feedback-form');
 const email = document.querySelector('[name="email"]');
 const message = document.querySelector('[name="message"]');
+const submitBtn = document.querySelector('button');
+submitBtn.disabled = true;
 
-const inputData = {}
+const inputData = {email:'', message:''} || {};
 
 form.addEventListener('submit', onFormSubmit);
-form.addEventListener('input', throttle(onEmailInput, 500));
+form.addEventListener('input', throttle(onInput, 500));
   
 populateData();
 
 function onFormSubmit(evt) {
-    evt.preventDefault();
+    evt.preventDefault(); 
+    submitBtn.disabled = true;
+
+    console.log(inputData)
 
     evt.currentTarget.reset();
-    localStorage.removeItem(STORAGE_KAY);
+    localStorage.removeItem(STORAGE_KEY);
+
+  
 }
 
-function onEmailInput(evt) {
+function onInput(evt) {
 
-    inputData[evt.target.name] = evt.target.value;
-   
-   localStorage.setItem(STORAGE_KAY, JSON.stringify(inputData));
+   inputData[evt.target.name] = evt.target.value
+
+   localStorage.setItem(STORAGE_KEY, JSON.stringify(inputData));
+
+   if (email.value && message.value) {
+    submitBtn.disabled = false;
+};
+
+
 
 }
-
-
 
 function populateData() {
   
-    const savedInputData = localStorage.getItem(STORAGE_KAY);
+    const savedInputData = localStorage.getItem(STORAGE_KEY);
     const parsedInputData = JSON.parse(savedInputData);
    
+     if(parsedInputData) {
+        for (const key in parsedInputData){
 
-     
-    if(parsedInputData) {
- 
-        email.value = parsedInputData.email;
-         message.value = parsedInputData.message;
-    }
+        if(parsedInputData.hasOwnProperty(key)) {
+        email.value = parsedInputData.email || ''; 
+         message.value = parsedInputData.message || '';
+        }
+    } 
+    if (email.value && message.value) {
+        submitBtn.disabled = false;
+    };
+    
 }
-   
+     
+};
