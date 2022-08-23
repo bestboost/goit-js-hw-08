ca8bc106ba6fa6fccf60d5d7947d7bd812e2f9bd
Darya Hudzovska <1bestboost@gmail.com>
2022-08-23 16:16:11 +0300

    add UX task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,47 +1,64 @@
 import throttle from 'lodash.throttle';
 
-const STORAGE_KAY = 'feedback-form-state';
+const STORAGE_KEY = 'feedback-form-state';
 
 const form = document.querySelector('.feedback-form');
 const email = document.querySelector('[name="email"]');
 const message = document.querySelector('[name="message"]');
+const submitBtn = document.querySelector('button');
+submitBtn.disabled = true;
 
-const inputData = {};
+const inputData =  {};
 
 form.addEventListener('submit', onFormSubmit);
 form.addEventListener('input', throttle(onInput, 500));
   
 populateData();
+console.log ('func', populateData())
 
 function onFormSubmit(evt) {
     evt.preventDefault(); 
+    submitBtn.disabled = true;
+    console.log('отправляем форму', inputData);
 
     evt.currentTarget.reset();
-    localStorage.removeItem(STORAGE_KAY);
+    localStorage.removeItem(STORAGE_KEY);
 
-    console.log(inputData);
+  
 }
 
 function onInput(evt) {
-  
+
    inputData[evt.target.name] = evt.target.value
-   
-   localStorage.setItem(STORAGE_KAY, JSON.stringify(inputData));
+   console.log('Input', inputData)
+   localStorage.setItem(STORAGE_KEY, JSON.stringify(inputData));
+
+   if (email.value && message.value) {
+    submitBtn.disabled = false;
+};
+
+
 
 }
 
 function populateData() {
   
-    const savedInputData = localStorage.getItem(STORAGE_KAY);
+    const savedInputData = localStorage.getItem(STORAGE_KEY);
     const parsedInputData = JSON.parse(savedInputData);
    
      if(parsedInputData) {
-        email.value = parsedInputData.email; 
-         message.value = parsedInputData.message;
-      
-    } 
-        email.value = ''; 
-        message.value = '';
+        for (const key in parsedInputData){
 
+        if(parsedInputData.hasOwnProperty(key)) {
+        email.value = parsedInputData.email || ''; 
+         message.value = parsedInputData.message || '';
+        }
+    } 
+    if (email.value && message.value) {
+        submitBtn.disabled = false;
+    };
+    
+}
+     
 };
 
